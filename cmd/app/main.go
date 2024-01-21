9475939187d1fb4d0c227c3d139e9e799b520373
dokshiye <okshieva@gmail.com>
2024-01-21 21:37:19 +0600

    Adding connection to DB

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "api-service/internal/app"
+
 func main(){
-	
+	app.Run()
 }
--- internal/app/run.go
+++ internal/app/run.go
@@ -1,6 +1,38 @@
 package app
 
-func Run(){
-	//db
-	repo := repository.NewRepository(db)
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Run() {
+	// db
+	// TODO: logs
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connString := fmt.Sprintf("user=%v password=%v dbname=%v sslmode=disable", dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("PostgreSQL!")
+	// repo := repository.NewRepository(db)
 }
